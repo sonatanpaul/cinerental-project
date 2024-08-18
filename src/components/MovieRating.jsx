cc50bfb2d9ed450b2e891787dd506aa14229a616
sonatanpaul <iamsonatanpaul@gmail.com>
2024-08-18 20:18:42 +0600

    movie card rating issue fixed

---
+++ src/components/MovieRating.jsx
@@ -0,0 +1,12 @@
+import Star from "../assets/star.svg";
+
+export default function MovieRating({ value }) {
+  const stars = Array(value).fill(Star);
+  return (
+    <>
+      {stars.map((star, index) => (
+        <img key={index} src={star} alt="star" className="h-5 w-5" />
+      ))}
+    </>
+  );
+}
--- src/components/cinemovie/MovieCard.jsx
+++ src/components/cinemovie/MovieCard.jsx
@@ -1,5 +1,6 @@
 /* eslint-disable react/prop-types */
 import { getImgUrl } from "../../utils/cine-utility";
+import MovieRating from "../MovieRating";
 export default function MovieCard({ movie }) {
   return (
     <>
@@ -13,11 +14,7 @@ export default function MovieCard({ movie }) {
           <h3 className="text-xl mb-1">{movie.title}</h3>
           <p className="text-[#575A6E] text-sm mb-2">{movie.genre}</p>
           <div className="flex items-center space-x-1 mb-5">
-            <img src="./assets/star.svg" width="14" height="14" alt="" />
-            <img src="./assets/star.svg" width="14" height="14" alt="" />
-            <img src="./assets/star.svg" width="14" height="14" alt="" />
-            <img src="./assets/star.svg" width="14" height="14" alt="" />
-            <img src="./assets/star.svg" width="14" height="14" alt="" />
+            <MovieRating value={movie.rating} />
           </div>
           <a
             className="bg-primary rounded-lg py-2 px-5 flex items-center justify-center gap-2 text-[#171923] font-semibold text-sm"
